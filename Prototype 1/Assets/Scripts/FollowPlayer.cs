b509a2d5331a2969a54adc934075d24728735bbe
William Barnes <jakebarnes1989@gmail.com>
2021-01-23 13:24:29 -0500

    Initial Commit
    Initial commit for Create With Code project. Version Control implemented at project lesson 1.2 completed.

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    public int camOffsetY = 10;
+    public int camOffsetZ = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + (Vector3.up * camOffsetY) + (Vector3.back * camOffsetZ);
+    }
+}
