9e917efd18ccb80f6451e763b3ca9e36446f8cac
William Barnes <jakebarnes1989@gmail.com>
2021-01-23 16:48:23 -0500

    Player Controller and InputMan Update
    - Broke out movement logic into MoveForward function.
    - Add boost functionality
    - Changed Fire3 input name to Boost

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Vehicle speed modifier
-    public float speed = 5.0f;
-    public float turnSpeed = 1.0f;
+    public float speed = 10.0f;
+    public float boostSpeed = 10.0f;
+    public float turnSpeed = 10.0f;
     public float horizontalInput = 0.0f;
     public float verticalInput = 0.0f;
 
@@ -24,8 +25,22 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        MoveForward();
 
+    }
+
+    void MoveForward()
+    {
+        // Handle player input and forward movement
+        if(Input.GetButton("Boost"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * boostSpeed * verticalInput);
+        } else
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        }
+
+        // Handle player input and turn movement
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
