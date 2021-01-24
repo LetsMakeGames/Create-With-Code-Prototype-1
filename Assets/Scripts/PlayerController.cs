1b8730cd68d0919fad5030a5fce3e5442c59a377
William Barnes <jakebarnes1989@gmail.com>
2021-01-23 19:11:45 -0500

    Plane Challenge (#2)
    * Update PlayerController.cs
    
    - Updated input var name to horizontal input to be more clear.
    - Added vertical input.
    
    * Updated Build Settings
    
    Switched to WebGL Platform and updated build settings.
    
    * Player Controller and InputMan Update
    
    - Broke out movement logic into MoveForward function.
    - Add boost functionality
    - Changed Fire3 input name to Boost
    
    * Plane Challenge
    
    - Imported Plane Challenge
    - Fixed plane flying backwards
    - Fixed plane rotating without player input
    - Fixed plan speed
    - Fixed camera not following player
    - Fixed camera position and rotation
    - Added properller rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Vehicle speed modifier
-    public float speed = 5.0f;
-    public float turnSpeed = 1.0f;
-    public float input = 0.0f;
+    // Initalize variables
+    private float speed = 20.0f;
+    private float boostSpeed = 40.0f;
+    private float turnSpeed = 10.0f;
+    private float horizontalInput = 0.0f;
+    private float verticalInput = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +21,26 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Get Player Input
-        input = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * input);
+        MoveForward();
+
+    }
+
+    void MoveForward()
+    {
+        // Checks for boost button held down, then handle player input and forward movement
+        if (Input.GetButton("Boost"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * boostSpeed * verticalInput);
+        } else
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        }
+
+        // Handle player input and turn movement
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
